a2d1a835a9a6c5727f76b462a324f3fc28d4278a
jack <knight131991@gmail.com>
2018-08-28 17:33:44 +0800

    add animation sample 1

--- js-learning/MyFirstProgram/scripts/MainFrame.js
+++ js-learning/MyFirstProgram/scripts/MainFrame.js
@@ -1,12 +1,38 @@
-class buttonCreater{
-    constructor(name){
-        this.name = name;
-        this.element = jQuery('<button></button>');
-        this.element.text(name);
-        return this.element;
-    };
+class JButtonCreator{
+    constructor(name, className){
+        this._$button = jQuery('<button></button>');
+        this._$button.text(name);
+        this._$button.addClass(className);
+    }
 
-};
+    registerSpecifyEvent(url){
+        const func = ()=>{
+            document.getElementById("myiframe").src = url;
+        }
+          this._$button.on("click", func);
+    }
 
-let button1 = new buttonCreater('click me!!');
-$('nav').append(button1);
+    get $button(){
+        return this._$button;
+    }
+
+}
+
+class JLabelCreator{
+    constructor(text){
+        this._$label = jQuery('<h3></h3>');
+        this._$label.text(text);
+    }
+
+    get $label(){
+        return this._$label;
+    }
+}
+
+const label1 = new JLabelCreator('Animation Example');
+
+const button1 = new JButtonCreator('Animation Sample 1', 'mybutton');
+button1.registerSpecifyEvent("AnimationSimple1.html");
+
+$('nav').append(label1.$label);
+$('nav').append(button1.$button);
