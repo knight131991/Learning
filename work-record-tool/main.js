f7ca1412cb762797f0985a4bac747112f7c1b2f3
TZU.CHIEH.LIN <TZU.CHIEH.LIN@deltaww.com>
2021-01-05 15:04:18 +0800

    write the feature of auto-launch

--- work-record-tool/main.js
+++ work-record-tool/main.js
@@ -89,5 +89,11 @@ app.on("window-all-closed", function () {
 
 app.setAppUserModelId(process.execPath);
 
+app.setLoginItemSettings({
+  openAtLogin: true,
+  path: process.execPath,
+  name: path.basename(process.execPath),
+});
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
